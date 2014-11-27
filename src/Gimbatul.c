e75f9e0a89e6e6381982c023b3252a8771a81e31
Jesse Millar <hellojessemillar@gmail.com>
2014-11-26 20:14:51 -0800

    Chooses a random image on startup

--- src/Gimbatul.c
+++ src/Gimbatul.c
@@ -7,6 +7,14 @@ MenuLayer *g_menu_layer;
 BitmapLayer *g_image_layer;
 GBitmap *g_image;
 
+int g_image_count = 4;
+uint32_t g_images[4] = {
+	RESOURCE_ID_CORRIDOR_CONTINUE,
+	RESOURCE_ID_CORRIDOR_DEAD,
+	RESOURCE_ID_CORRIDOR_LEFT,
+	RESOURCE_ID_CORRIDOR_RIGHT
+};
+
 void step(struct tm *tick_time, TimeUnits units_changed)
 {
 	// Do random step generation stuff
@@ -15,13 +23,15 @@ void step(struct tm *tick_time, TimeUnits units_changed)
 void window_load(Window *window)
 {
 	g_image_layer = bitmap_layer_create(GRect(0, 0, 144, 168));
-	g_image = gbitmap_create_with_resource(RESOURCE_ID_CORRIDOR_CONTINUE);
+	g_image = gbitmap_create_with_resource(g_images[rand() % g_image_count]);
 	bitmap_layer_set_bitmap(g_image_layer, g_image);
 	layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(g_image_layer));
 }
 
 void window_unload(Window *window)
 {	
+	menu_layer_destroy(g_menu_layer);
+
 	bitmap_layer_destroy(g_image_layer);
 	gbitmap_destroy(g_image);
 }
@@ -35,6 +45,8 @@ void init()
 	});
 
 	tick_timer_service_subscribe(MINUTE_UNIT, (TickHandler)step); // Ask for an update every minute
+
+	srand(time(NULL)); // Set the random seed to the current time
 
 	window_set_fullscreen(g_window, true); // Make the app fullscreen
 	window_stack_push(g_window, true);
