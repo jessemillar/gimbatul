9f9ab5ff95006248704d392c37491f2ca68c8822
Jesse Millar <hellojessemillar@gmail.com>
2014-11-28 15:25:34 -0800

    Showing a fake time now

--- src/Gimbatul.c
+++ src/Gimbatul.c
@@ -9,18 +9,20 @@ BitmapLayer *g_image_layer;
 GBitmap *g_image;
 Layer *g_health_bars_layer;
 
-int g_image_count = 10; // I'm too lazy to figure out a better way to do this
-uint32_t g_images[10] = {
+TextLayer *g_clock_layer;
+GFont g_font_sailor_stitch;
+
+int g_image_count = 4; // I'm too lazy to figure out a better way to do this
+uint32_t g_images[4] = {
 	RESOURCE_ID_CORRIDOR_CONTINUE,
 	RESOURCE_ID_CORRIDOR_DEAD,
 	RESOURCE_ID_CORRIDOR_LEFT,
 	RESOURCE_ID_CORRIDOR_RIGHT,
-	RESOURCE_ID_ENEMY_SKELLY,
-	RESOURCE_ID_ENEMY_SLUG,
-	RESOURCE_ID_ENEMY_SPIDER,
-	RESOURCE_ID_ENEMY_TROLL,
-	RESOURCE_ID_OTHER_CHEST,
-	RESOURCE_ID_OTHER_PARCHMENT
+	// RESOURCE_ID_ENEMY_SKELLY,
+	// RESOURCE_ID_ENEMY_SLUG,
+	// RESOURCE_ID_ENEMY_SPIDER,
+	// RESOURCE_ID_ENEMY_TROLL,
+	// RESOURCE_ID_OTHER_CHEST
 };
 
 void draw_health(Layer *me, GContext *ctx) // Currently draws two lines instead of one square for thickness (ghetto)
@@ -45,7 +47,7 @@ void heal_player(int amount)
 			g_player_current_health = g_player_max_health;
 		}
 
-		layer_mark_dirty(g_health_bars_layer); // Mark dirty to force a redraw of the whole screen (even though it looks like we're just redrawing the health bars)
+		layer_mark_dirty(g_health_bars_layer); // Mark dirty to force a redraw the health bars
 	}
 }
 
@@ -60,7 +62,7 @@ void hurt_player(int amount)
 			g_player_current_health = 0;
 		}
 
-		layer_mark_dirty(g_health_bars_layer); // Mark dirty to force a redraw of the whole screen (even though it looks like we're just redrawing the health bars)
+		layer_mark_dirty(g_health_bars_layer); // Mark dirty to force a redraw the health bars
 	}
 }
 
@@ -71,7 +73,7 @@ void take_step(struct tm *tick_time, TimeUnits units_changed)
 	g_image = gbitmap_create_with_resource(g_images[rand() % g_image_count]); // Select a random image from the array
 	bitmap_layer_set_bitmap(g_image_layer, g_image);
 
-	layer_mark_dirty(g_health_bars_layer); // Mark dirty to force a redraw of the whole screen (even though it looks like we're just redrawing the health bars)
+	layer_mark_dirty(bitmap_layer_get_layer(g_image_layer)); // Mark dirty to force a redraw of the image
 }
 
 void window_load(Window *window)
@@ -84,6 +86,15 @@ void window_load(Window *window)
 	g_health_bars_layer = layer_create(GRect(0, 0, 144, 168));
 	layer_set_update_proc(g_health_bars_layer, draw_health); // Set the drawing context for health bars
 	layer_add_child(window_get_root_layer(g_window), g_health_bars_layer);
+
+	g_font_sailor_stitch = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_ALAGARD_15));
+	g_clock_layer = text_layer_create(GRect(0, 142, 144, 30));
+	text_layer_set_background_color(g_clock_layer, GColorClear);
+	text_layer_set_text_color(g_clock_layer, GColorBlack);
+	text_layer_set_font(g_clock_layer, g_font_sailor_stitch);
+	text_layer_set_text_alignment(g_clock_layer, GTextAlignmentCenter);
+	text_layer_set_text(g_clock_layer, "12:24");
+	layer_add_child(window_get_root_layer(g_window), text_layer_get_layer(g_clock_layer));
 }
 
 void window_unload(Window *window)
@@ -91,6 +102,8 @@ void window_unload(Window *window)
 	bitmap_layer_destroy(g_image_layer);
 	gbitmap_destroy(g_image);
 	layer_destroy(g_health_bars_layer);
+	text_layer_destroy(g_clock_layer);
+	fonts_unload_custom_font(g_font_sailor_stitch);
 }
 
 void init()
