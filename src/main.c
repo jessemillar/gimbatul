ad414d039af8c159c8a7244bc6781efe60e579e1
Jesse Millar <hellojessemillar@gmail.com>
2014-12-02 10:52:50 -0700

    Adding images

--- src/main.c
+++ src/main.c
@@ -1,9 +1,11 @@
 #include <pebble.h>
 
+// Including as .h instead of .c allows for the above #include <pebble.h> to apply to all files and avoids some ugly errors
 #include <functions.h>
 #include <player.h>
 
-Window *g_window;
+Window *g_window_main;
+Window *g_window_menu;
 
 BitmapLayer *g_image_layer;
 GBitmap *g_image;
@@ -11,6 +13,10 @@ Layer *g_health_bars_layer;
 
 TextLayer *g_clock_layer;
 GFont g_font_alagard;
+
+int g_menu_size = 3;
+SimpleMenuLayer *g_menu;
+SimpleMenuItem *g_menu_items;
 
 int g_image_count = 4; // I'm too lazy to figure out a better way to do this
 uint32_t g_images[4] = {
@@ -69,7 +75,7 @@ void hurt_player(int amount)
 void take_step(struct tm *tick_time, TimeUnits units_changed)
 {
 	// Populate the clock
-	static char buffer[] = "00:00"; // Allocate "long-lived" storage (required by TextLayer)
+	static char buffer[] = "00:00xxx"; // Allocate "long-lived" storage (required by TextLayer)
 	strftime(buffer, sizeof(buffer), "%H:%M", tick_time); // Write the time to the buffer in a safe manner
 	clock_copy_time_string(buffer, sizeof(buffer)); // Reformat the time to the user's preference
 	text_layer_set_text(g_clock_layer, buffer); // Display the time in the text time layer
@@ -107,7 +113,7 @@ void click_config_provider(Window *window)
 	window_single_click_subscribe(BUTTON_ID_DOWN, button_down_handler);
 }
 
-void window_load(Window *window)
+void window_main_load(Window *window)
 {
 	g_image_layer = bitmap_layer_create(GRect(0, 0, 144, 168));
 	g_image = gbitmap_create_with_resource(RESOURCE_ID_CORRIDOR_CONTINUE); // Initially set the image so we have something to destroy in the step function
@@ -116,7 +122,7 @@ void window_load(Window *window)
 
 	g_health_bars_layer = layer_create(GRect(0, 0, 144, 168));
 	layer_set_update_proc(g_health_bars_layer, draw_health); // Set the drawing context for health bars
-	layer_add_child(window_get_root_layer(g_window), g_health_bars_layer);
+	layer_add_child(window_get_root_layer(g_window_main), g_health_bars_layer);
 
 	g_font_alagard = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_ALAGARD_15));
 	g_clock_layer = text_layer_create(GRect(4, 150, 72, 30));
@@ -124,29 +130,71 @@ void window_load(Window *window)
 	text_layer_set_text_color(g_clock_layer, GColorBlack);
 	text_layer_set_font(g_clock_layer, g_font_alagard);
 	// text_layer_set_text_alignment(g_clock_layer, GTextAlignmentCenter);
-	layer_add_child(window_get_root_layer(g_window), text_layer_get_layer(g_clock_layer));
+	layer_add_child(window_get_root_layer(g_window_main), text_layer_get_layer(g_clock_layer));
 
 	populate_clock();
 }
 
-void window_unload(Window *window)
+void window_main_unload(Window *window)
 {	
 	bitmap_layer_destroy(g_image_layer);
 	gbitmap_destroy(g_image);
 	layer_destroy(g_health_bars_layer);
 	text_layer_destroy(g_clock_layer);
 	fonts_unload_custom_font(g_font_alagard);
+	simple_menu_layer_destroy(g_menu);
+}
+
+void window_menu_load(Window *window)
+{
+	// menu_icon_image = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_MENU_ICON_1);
+
+	// Although we already defined NUM_FIRST_MENU_ITEMS, you can define an int as such to easily change the order of menu items later
+	int num_a_items = 0;
+
+	g_menu_items[num_a_items++] = (SimpleMenuItem)
+	{
+		// You should give each menu item a title and callback
+		.title = "First Item",
+		// .callback = menu_select_callback,
+	};
+
+	g_menu_items[num_a_items++] = (SimpleMenuItem)
+	{
+		.title = "Second Item",
+		.subtitle = "Here's a subtitle",
+		// .callback = menu_select_callback,
+	};
+
+	g_menu_items[num_a_items++] = (SimpleMenuItem)
+	{
+		.title = "Third Item",
+		.subtitle = "This has an icon",
+		// .callback = menu_select_callback,
+		// .icon = menu_icon_image
+	};
+}
+
+void window_menu_unload(Window *window)
+{	
+	// Stuff
 }
 
 void init()
 {
-	g_window = window_create();
-	window_set_window_handlers(g_window, (WindowHandlers) {
-		.load = window_load,
-		.unload = window_unload,
+	g_window_main = window_create();
+	window_set_window_handlers(g_window_main, (WindowHandlers) {
+		.load = window_main_load,
+		.unload = window_main_unload,
 	});
 
-	window_set_click_config_provider(g_window, (ClickConfigProvider) click_config_provider);
+	g_window_menu = window_create();
+	window_set_window_handlers(g_window_menu, (WindowHandlers) {
+		.load = window_menu_load,
+		.unload = window_menu_unload,
+	});
+
+	window_set_click_config_provider(g_window_main, (ClickConfigProvider) click_config_provider);
 	tick_timer_service_subscribe(MINUTE_UNIT, (TickHandler)take_step);
 
 	srand(time(NULL)); // Set the random seed to the current time
@@ -154,8 +202,8 @@ void init()
 	// Load persistent values
 	g_player_current_health = persist_exists(NUM_PLAYER_CURRENT_HEALTH) ? persist_read_int(NUM_PLAYER_CURRENT_HEALTH) : 100;
 
-	window_set_fullscreen(g_window, true); // Make the app fullscreen
-	window_stack_push(g_window, true);
+	window_set_fullscreen(g_window_main, true); // Make the app fullscreen
+	window_stack_push(g_window_main, true);
 }
 
 void deinit()
@@ -163,7 +211,7 @@ void deinit()
 	// Save persistent values
 	persist_write_int(NUM_PLAYER_CURRENT_HEALTH, g_player_current_health);
 
-	window_destroy(g_window);
+	window_destroy(g_window_main);
 }
 
 int main(void)
